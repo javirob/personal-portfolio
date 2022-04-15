17baa6f17e0b8dac20faed311b85307264ba7c27
javier <javi.spanish.dev@gmail.com>
2022-04-15 11:50:47 +0100

    project added

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,9 +1,18 @@
-import project1 from '../../assets/coding-gym.png';
-import project2 from '../../assets/project2.png';
+import project1 from '../../assets/project-1.jpg';
+import project2 from '../../assets/project-2.png';
+import project3 from '../../assets/project-3.png';
 
 const projectsData = [
   {
     id: 'p01',
+    img: project1,
+    title: 'HTML/CSS/JS',
+    desc: ['Responsive', 'HTML/CSS', 'JavaScript', 'JQuery'],
+    siteUrl: 'https://hair-academy.netlify.app/',
+    githubUrl: 'https://github.com/javirob/hair-school',
+  },
+  {
+    id: 'p02',
     img: project2,
     title: 'Bootstrap5',
     desc: ['Landing Page', 'Bootstrap5', 'JavaScript', 'AOS'],
@@ -12,7 +21,7 @@ const projectsData = [
   },
   {
     id: 'p02',
-    img: project1,
+    img: project3,
     title: 'React.js',
     desc: ['Landing Page', 'HTML/CSS', 'JavaScript', 'React'],
     siteUrl: 'https://coding-gym.netlify.app/',
